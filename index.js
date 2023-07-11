372dac06582fb78c7793c4796f44ecfcec1bda35
Victor <wanjala94vic@gmail.com>
2023-07-11 23:41:17 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+let foo = function () {
+    return 'bar';
+};
+
+let divide = function() {
+    return 2000/100;
+};
+
+let square = (num) => {
+    return num*num;
+};
+
+let add = (num1, num2) => {
+    return num1 + num2;
+};
